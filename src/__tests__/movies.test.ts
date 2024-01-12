f52a8672f4f5d3f40d4506611dcc36e5946bb607
Maksym Sihua <maxsiguaua@gmail.comgit config --global user.name Maksym>
2024-01-12 18:01:57 +0200

    test for review 2

--- src/__tests__/movies.test.ts
+++ src/__tests__/movies.test.ts
@@ -1,11 +1,49 @@
 import request from 'supertest';
 import { app } from '../index';
+import { Movie } from '../schemas/movies';
+import { error } from 'console';
 
 describe('Movies API', () => {
   let addedMovieId: string;
+  const fakeMovies = [{
+    "_id": {
+      "$oid": "6520106252da1e7daaf37dee"
+    },
+    "title": "Avatar",
+    "genre": [
+      "Action, Adventure, Fantasy"
+    ],
+    "releaseDate": "18 Dec 2009",
+    "description": "garbage film"
+  },
+  {
+    "_id": {
+      "$oid": "6520106252da1e7daaf37def"
+    },
+    "title": "Lord of the Rings",
+    "genre": [
+      "Action"
+    ],
+    "releaseDate": "19 Dec 2001",
+    "description": "best film ever"
+  },
+  {
+    "_id": {
+      "$oid": "6520106252da1e7daaf37df0"
+    },
+    "title": "Avatar: The Way of Water",
+    "genre": [
+      "Action, Adventure, Fantasy"
+    ],
+    "releaseDate": "06 Dec 2022",
+    "description": "very-very garbage film"
+  }];
 
   it('should get a list of all movies', async () => {
     const response = await request(app).get('/movies');
+
+    Movie.find = jest.fn().mockResolvedValue(fakeMovies);
+
     expect(response.status).toBe(200);
     expect(response.body).toBeDefined();
     expect(response.body).toBeInstanceOf(Array);
@@ -13,19 +51,28 @@ describe('Movies API', () => {
 
   it('should not get a list of all movies and return 404 error', async () => {
     const response = await request(app).get('/moviess');
+
+    Movie.find = jest.fn().mockRejectedValue(new Error('Async error'));
+
     expect(response.status).toBe(404);
     expect(response.body).toBeDefined();
   });
 
   it('should get a list of movies by genre', async () => {
+    Movie.find = jest.fn().mockResolvedValue(fakeMovies);
+
     const response = await request(app).get('/movies/genres/Action');
+
     expect(response.status).toBe(200);
     expect(response.body).toBeDefined();
     expect(response.body).toBeInstanceOf(Array);
   });
 
   it('should return an empty array for a non-existent genre', async () => {
+    Movie.find = jest.fn().mockRejectedValue(new Error('Async error'));
+
     const response = await request(app).get('/movies/genres/NonExistentGenre');
+
     expect(response.status).toBe(404);
     expect(response.body).toBeDefined();
   });
@@ -38,11 +85,15 @@ describe('Movies API', () => {
       description: 'A new movie.',
     };
 
+    Movie.create = jest.fn().mockResolvedValueOnce(newMovie);
+    Movie.find = jest.fn().mockResolvedValue([newMovie]);
+
     const response = await request(app).post('/movies').send(newMovie);
     expect(response.status).toBe(201);
     expect(response.body).toBeDefined();
     expect(response.body).toBeInstanceOf(Array);
 
+    // Store the added movie's ID for cleanup
     addedMovieId = response.body[0]._id;
   });
 
@@ -54,25 +105,40 @@ describe('Movies API', () => {
       descriptions: 'A new movie.',
     };
 
+    Movie.create = jest.fn().mockRejectedValue(new Error('Async error'));
+
     const response = await request(app).post('/movies').send(newMovie);
     expect(response.status).toBe(400);
   });
 
   it('should update a movie by title', async () => {
-    const updatedMovie = {
-      title: 'Updated Movie',
+    const movie = {
+      title: 'Movie',
       genre: ['Action'],
       releaseDate: '2023-01-02',
       description: 'An updated movie.',
     };
 
-    const response = await request(app).put(`/movies/NewMovie`).send(updatedMovie);
+    const editedMovie = {
+      "title": movie.title + '1',
+      "genre": movie.genre,
+      "releaseDate": movie.releaseDate,
+      "description": movie.description
+  };
+
+    Movie.findOne = jest.fn().mockResolvedValue(movie);
+    Movie.findOneAndUpdate = jest.fn().mockResolvedValue(editedMovie);
+    Movie.find = jest.fn().mockResolvedValue([editedMovie]);
+
+    const response = await request(app).put(`/movies/NewMovie`).send(editedMovie);
     expect(response.status).toBe(200);
     expect(response.body).toBeDefined();
     expect(response.body).toBeInstanceOf(Array);
   });
 
   it('should handle updating a non-existent movie', async () => {
+    Movie.findOneAndUpdate = jest.fn().mockRejectedValue(new Error('Async error'));
+
     const response = await request(app).put('/movies/NonExistentMovie').send({
       title: 'Updated Movie',
       genre: ['Action'],
@@ -84,8 +150,19 @@ describe('Movies API', () => {
   });
 
   it('should delete a movie by ID', async () => {
-    const moviesBeforeDelete = await request(app).get('/movies');
-    const idToDelete = moviesBeforeDelete.body[0]._id;
+    const movie = {
+      "_id": "6520106252da1e7daaf37dee",
+      "title": "Avatar",
+      "genre": [
+        "Action, Adventure, Fantasy"
+      ],
+      "releaseDate": "18 Dec 2009",
+      "description": "garbage film"
+    }
+
+    Movie.findByIdAndDelete = jest.fn().mockResolvedValue(movie);
+
+    const idToDelete = "6520106252da1e7daaf37dee";
 
     const response = await request(app).delete(`/movies/${idToDelete}`);
     expect(response.status).toBe(200);
@@ -93,15 +170,19 @@ describe('Movies API', () => {
     expect(response.body).toBeInstanceOf(Object);
   });
 
-  it('should delete a movie by ID', async () => {
+  it('should not delete a movie by ID when id is wrong and retuen 404', async () => {
     const fakeId = '6520106252da1e7daaf37d32'
+
+    Movie.findByIdAndDelete = jest.fn().mockRejectedValue(new Error('wrong ID'));
 
     const response = await request(app).delete(`/movies/${fakeId}`);
     expect(response.status).toBe(404);
     expect(response.body).toBeDefined();
   });
 
-  it('should delete a movie by ID', async () => {
+  it('should not delete a movie by ID when ID is not provided', async () => {
+    Movie.findByIdAndDelete = jest.fn().mockRejectedValue(new Error('no ID was provided'));
+
     const response = await request(app).delete(`/movies/`);
     expect(response.status).toBe(404);
     expect(response.body).toBeDefined();
